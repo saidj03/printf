38506fe684011a47d5805c570c9f51569c198f85
saidj03 <jsaidali98@gmail.com>
2023-11-16 13:54:42 +0300

    printf project

--- print_reverse.c
+++ print_reverse.c
@@ -12,11 +12,6 @@ int print_revs(va_list args)
 	int len = strlen(str);
 	int i;
 
-	if (str == NULL)
-	str = '(null)';
-
-	while (str[len] != '\0')
-		len++;
 	for (i = len - 1; i >= 0; i--)
 		_putchar(str[i]);
 	return (len);
